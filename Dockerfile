1c39796700fd4c87519b3a9739930194d2e58978
khanhph4 <khanhph4@fpt.com.vn>
2024-04-22 16:58:56 +0700

    init code

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM python:3.12
+ADD . /app
+WORKDIR /app
+RUN python -m pip install --upgrade pip 
+RUN pip install --upgrade pipenv
+RUN pipenv install --system --deploy
+
---
+++ main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import sys
+from logzero import logger
+
+if len(sys.argv) > 1:
+    arg = sys.argv[1]
+    logger.info("This is a simple helloworld! Hello {}".format(arg))
+else:
+    arg = None
+    logger.info("This is a simple helloworld! You did not pass an arg")
+
+
+def main():
+    with open("test.txt", "w") as f:
+        f.write(f"Hello World, {arg}!\n")
+
+if __name__ == "__main__":
+    main()
